a478e74346d3e198f464fb8fe54cf7d417ca04a0
Willian Oliveira <willian.oliveira737@gmail.com>
2024-05-27 15:34:06 -0300

    Melhora transformacao

--- src/transformacao/main.py
+++ src/transformacao/main.py
@@ -2,50 +2,46 @@ import pandas as pd
 import sqlite3
 from datetime import datetime
 
-# Definir o caminho para o arquivo JSONL
+# Ler os dados do arquivo JSON
 jsonl_path = '../data/data.jsonl'
-
-# Ler os dados do arquivo JSONL
 df = pd.read_json(jsonl_path, lines=True)
 
-# Exibir o DataFrame resultante
 print(df.head())
 
-# Adicionar a coluna _source com um valor fixo
+# Adicionar a coluna source com o valor de onde os dados foram extraídos;
 df['_source'] = "https://lista.mercadolivre.com.br/tenis-corrida-masculino"
 
-# Adicionar a coluna _data_coleta com a data e hora atuais
+# Adicionar a coluna data_coleta com a data e hora atual;
 df['_data_coleta'] = datetime.now()
 
-# Tratar os valores nulos para colunas numéricas e de texto
+# Alterar valores de colunas para o tipo Float:
 df['old_price_reais'] = df['old_price_reais'].fillna(0).astype(float)
 df['old_price_centavos'] = df['old_price_centavos'].fillna(0).astype(float)
 df['new_price_reais'] = df['new_price_reais'].fillna(0).astype(float)
 df['new_price_centavos'] = df['new_price_centavos'].fillna(0).astype(float)
 df['reviews_rating_number'] = df['reviews_rating_number'].fillna(0).astype(float)
 
-# Remover os parênteses das colunas `reviews_amount`
+# Alterar valores de colunas para o tipo Integer e Remover possíveis espaços
 df['reviews_amount'] = df['reviews_amount'].str.replace('[\(\)]', '', regex=True)
 df['reviews_amount'] = df['reviews_amount'].fillna(0).astype(int)
 
-# Tratar os preços como floats e calcular os valores totais
+#Remover linhas que não possuem Marcas cadastradas (igual a null);
+df.dropna(subset=['brand'], inplace=True)
+
+# Criar coluna “old_price” -> Junção de coluna old_price_reais + old_price_centavos;
 df['old_price'] = df['old_price_reais'] + df['old_price_centavos'] / 100
+
+#Criar coluna “new_price” -> Junção de coluna new_price_reais + new_price_centavos;
 df['new_price'] = df['new_price_reais'] + df['new_price_centavos'] / 100
 
-# Remover as colunas antigas de preços
+# Remover colunas 'old_price_reais', 'old_price_centavos', 'new_price_reais' e  'new_price_centavos';
 df.drop(columns=['old_price_reais', 'old_price_centavos', 'new_price_reais', 'new_price_centavos'], inplace=True)
 
-# Conectar ao banco de dados SQLite (ou criar um novo)
-conn = sqlite3.connect('../data/quotes.db')
-
 # Salvar o DataFrame no banco de dados SQLite
+conn = sqlite3.connect('../data/quotes.db')
 df.to_sql('mercadolivre_items', conn, if_exists='replace', index=False)
-
-# Fechar a conexão com o banco de dados
 conn.close()
 
-# Configurar pandas para mostrar todas as colunas
-pd.options.display.max_columns = None
 
-# Exibir o DataFrame resultante
+# pd.options.display.max_columns = None
 print(df.head())
