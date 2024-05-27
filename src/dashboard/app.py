7923184a7bed5580b85d3a3991c6db03d0ddee9f
Willian Oliveira <willian.oliveira737@gmail.com>
2024-05-27 16:05:56 -0300

    Melhora dashboard

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -4,47 +4,43 @@ import sqlite3
 
 # Conectar ao banco de dados SQLite
 conn = sqlite3.connect('../data/quotes.db')
-
-# Carregar os dados da tabela 'mercadolivre_items' em um DataFrame pandas
 df = pd.read_sql_query("SELECT * FROM mercadolivre_items", conn)
-
-# Fechar a conexão com o banco de dados
 conn.close()
 
-# Título da aplicação
+# Título
 st.title('Pesquisa de Mercado - Tênis Esportivos no Mercado Livre')
 
-# Melhorar o layout com colunas para KPIs
+# Melhorar o layout com colunas
 st.subheader("KPIs Principais")
 col1, col2, col3 = st.columns(3)
 
-# KPI 1: Número total de itens
+# Número total de itens
 total_items = df.shape[0]
 col1.metric(label="Número Total de Itens", value=total_items)
 
-# KPI 2: Número de marcas únicas
+# Número de marcas únicas
 unique_brands = df['brand'].nunique()
 col2.metric(label="Número de Marcas Únicas", value=unique_brands)
 
-# KPI 3: Preço médio novo (em reais)
+# Preço médio novo (em reais)
 average_new_price = df['new_price'].mean()
 col3.metric(label="Preço Médio Novo (R$)", value=f"{average_new_price:.2f}")
 
-# Quais marcas são mais encontradas até a 10ª página
+# Marcas são mais encontradas
 st.subheader('Marcas mais encontradas até a 10ª página')
 col1, col2 = st.columns([4, 2])
 top_10_pages_brands = df.head(500)['brand'].value_counts().sort_values(ascending=False)
 col1.bar_chart(top_10_pages_brands)
 col2.write(top_10_pages_brands)
 
-# Qual o preço médio por marca
+# Preço médio por marca
 st.subheader('Preço médio por marca')
 col1, col2 = st.columns([4, 2])
 average_price_by_brand = df.groupby('brand')['new_price'].mean().sort_values(ascending=False)
 col1.bar_chart(average_price_by_brand)
 col2.write(average_price_by_brand)
 
-# Qual a satisfação por marca
+# Satisfação
 st.subheader('Satisfação por marca')
 col1, col2 = st.columns([4, 2])
 df_non_zero_reviews = df[df['reviews_rating_number'] > 0]
